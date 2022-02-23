da09e31ecedb8d23e5f3cf147e229927ebce4dc6
Asus <Asus@DESKTOP-F0SLSJ8>
2022-02-23 18:51:53 +0300

    Add project files.

---
+++ EX1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число от 0 до 100");
+var userNumber = int.Parse(Console.ReadLine());
+if (userNumber >= 0 && userNumber <= 30)
+{
+    Console.WriteLine("Данное число в промежутке от 0 до 30");
+}else if (userNumber >= 31 && userNumber <= 60)
+{
+    Console.WriteLine("Данное число в промежутке от 31 до 60");
+}else if (userNumber >= 61 && userNumber <= 100)
+{
+    Console.WriteLine("Данное число в промежутке от 61 до 100");
+} else
+{
+    Console.WriteLine("Я такого числа не знаю.");
+}
---
+++ EX2/Program.cs
@@ -0,0 +1,15 @@
+﻿int startNumber = 200;
+string answer1 = "";
+string space = ", ";
+while (startNumber <= 500)
+{
+    if (startNumber % 17 == 0)
+    {
+        string to17th = startNumber.ToString();
+        answer1 += to17th + space;
+    }
+    startNumber++;
+}
+char[] end = { ',' , ' '};
+string answer = answer1.TrimEnd(end);
+Console.WriteLine(answer);
